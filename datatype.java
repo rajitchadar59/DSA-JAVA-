1817118787078f0eaf352c295b43273abd38c096
rajitchadar59 <rajitathiya123@gmail.com>
2025-06-02 16:58:39 +0530

    files added

---
+++ datatype.java
@@ -0,0 +1,13 @@
+public class datatype{
+    public static  void main(String[] args){
+     
+   byte b =10;
+   System.out.println(b);
+   
+   char c= 'c';
+   System.out.println(c);
+
+   boolean bool = true;  
+      System.out.println(bool);   
+    }
+}

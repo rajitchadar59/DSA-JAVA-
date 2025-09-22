b6c7269026771c165ab7e1b68507a51b8148ce32
rajitchadar59 <rajitathiya123@gmail.com>
2025-09-22 16:16:07 +0530

    trie files

---
+++ trie/LongestWordWithAllPrefix.java
@@ -0,0 +1,88 @@
+public class LongestWordWithAllPrefix {
+
+     static class Node {
+    Node children[]=new Node[26];
+    boolean eow=false;
+
+    Node(){
+      for(int i =0 ; i <26 ; i++){
+        children[i]=null;
+      }
+    }
+
+    }
+
+    public static Node root=new Node();
+
+    public static void insert(String word){
+      Node curr=root;
+
+      for(int level=0 ; level < word.length() ; level++){
+        int idx=word.charAt(level)-'a';
+        if(curr.children[idx]==null){
+          curr.children[idx]=new Node();
+        }
+     
+          curr=curr.children[idx];
+        
+      }
+
+      curr.eow=true;
+    }
+
+     public static boolean search(String key){
+        Node curr=root;
+         for(int level=0 ; level < key.length() ; level++){
+        int idx=key.charAt(level)-'a';
+        if(curr.children[idx]==null){
+            return false;
+         }
+     
+          curr=curr.children[idx];
+        
+      }
+    
+     return  curr.eow==true;
+
+    }
+
+    
+    public static String ans="";
+
+    public static void longestWord(Node root ,StringBuilder temp){
+    
+    if(root==null){
+        return;
+    }
+
+    for(int i =0 ; i < 26 ; i++){
+
+        if(root.children[i] != null && root.children[i].eow==true){
+            char ch = (char) (i+'a');
+            temp.append(ch);
+         if(temp.length() > ans.length()){
+            ans=temp.toString();
+         }
+
+         longestWord(root.children[i], temp);
+         //backtrack
+         temp.deleteCharAt(temp.length()-1);
+        }
+    }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+     String words[]={"a" ,"banana" ,"app","appl" ,"ap","apply","apple"};
+     
+     for(String word:words){
+        insert(word);
+     }
+       longestWord(root, new StringBuilder(""));
+       System.out.println("longest word with all prefixes :" + ans );
+
+    }
+}

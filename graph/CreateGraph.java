b1c998f94634db0a009b4fcf5456b9c9c03b09c1
rajitchadar59 <rajitathiya123@gmail.com>
2025-09-22 22:33:26 +0530

    graph basics

---
+++ graph/CreateGraph.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public  class CreateGraph{
+
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s ,int d , int w){
+            this.src=s;
+            this.dest=d;
+            this.wt=w;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        /*
+
+       (5)
+   0 ------- 1
+           /  \
+      (1) /    \(3)
+         /       \
+        2 ------- 3
+        |   (1)
+    (2) |
+        |    
+        4   
+          
+
+          */
+
+     int v=5;
+     ArrayList<Edge>[] graph= new ArrayList[v];// null at each index
+
+     for(int i = 0 ; i < v ; i++){
+        graph[i]= new ArrayList<>();
+     }
+     
+     // 0 vertex
+
+     graph[0].add(new Edge(0,1,5));
+
+     graph[1].add(new Edge(1,2,5));
+     graph[1].add(new Edge(1,2,1));
+     graph[1].add(new Edge(1,3,3));
+     
+     graph[2].add(new Edge(2,1,1));
+     graph[2].add(new Edge(2,3,1));
+     graph[2].add(new Edge(2,4,2));
+
+
+     graph[3].add(new Edge(3,1,3));
+     graph[3].add(new Edge(3,2,1));
+
+
+     //2's neghbours
+     for(int i =0 ; i < graph[3].size() ;i++){
+        Edge e= graph[3].get(i);
+        System.out.println(e.dest);
+
+     }
+
+
+    }
+}
---
+++ graph/HasPath.java
@@ -0,0 +1,89 @@
+
+import java.util.*;
+
+public class HasPath {
+
+    static class Edge {
+
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+
+    }
+
+    public static boolean haspath(ArrayList<Edge>[] graph, int src, int dest, boolean visited[]) {
+
+        if (src == dest) {
+            return true;
+        }
+        
+         visited[src] = true;
+
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+           
+            if (!visited[e.dest] && haspath(graph, e.dest, dest, visited)) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        /*
+     1 ---- 3
+    /       | \
+   0        |  5 ---- 6
+    \       | /
+     2 ---- 4
+
+         */
+
+        int v = 7;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        
+        boolean visited[] = new boolean[graph.length];
+
+        createGraph(graph);
+         System.out.println(haspath(graph, 0,5 , visited));
+    }
+}
